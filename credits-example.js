25cc23ad031463b05c1fc4eac4eae20de55729bc
yokselzok <yokselzok@gmail.com>
2018-09-24 23:56:48 +0300

    credits-example.js

---
+++ credits-example.js
@@ -0,0 +1,9 @@
+module.exports = {
+  loginUrl: 'https://www.livejournal.com/login.bml',
+  username_selector: '#user',
+  password_selector: '#lj_loginwidget_password',
+  submit_selector: '.lj_login_form .b-loginform-btn--auth',
+
+  username: 'NICK',
+  password: 'PASS'
+};
