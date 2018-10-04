cd5d1ea47f9524c8ca040201da1404f0af3e2287
yokselzok <yokselzok@gmail.com>
2018-10-04 16:32:20 +0300

    Update credits-example.js for multy envs

--- credits-example.js
+++ credits-example.js
@@ -1,9 +1,19 @@
 module.exports = {
-  loginUrl: 'https://www.livejournal.com/login.bml',
-  username_selector: '#user',
-  password_selector: '#lj_loginwidget_password',
-  submit_selector: '.lj_login_form .b-loginform-btn--auth',
-
-  username: 'NICK',
-  password: 'PASS'
+  selectors: {
+    username: '#user',
+    password: '#lj_loginwidget_password',
+    submit: '.lj_login_form .b-loginform-btn--auth',
+  },
+  env: {
+    prod: {
+      loginUrl: 'https://www.example.com/login.bml',
+      username: 'NICK',
+      password: 'PASS'
+    },
+    dev: {
+      loginUrl: 'https://another-site.com/login.bml',
+      username: 'NICK',
+      password: 'PASS'
+    }
+  }
 };
