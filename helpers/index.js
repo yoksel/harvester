d57ee15e99666ef05cdc598ad28a4e3eef299b17
yokselzok <yokselzok@gmail.com>
2018-10-05 02:35:03 +0300

    Add handling errors to makeLogin

--- helpers/index.js
+++ helpers/index.js
@@ -148,25 +148,54 @@ const writeAllFiles = ({visitedUrls, collectedUrls, tree}) => {
   }
 };
 
-const makeLogin = async (page, credits, currentEnv) => {
+const makeLogin = (page, credits, currentEnv) => {
+  console.log('Make login...');
+
   const {
     selectors,
     env
   } = credits;
+  const errorSelector = selectors.error;
 
-  await page.goto(env[currentEnv].loginUrl);
+  const makeLoginPromise = new Promise(async (resolve, reject) => {
+    await page.goto(env[currentEnv].loginUrl);
 
-  await page.click(selectors.username);
-  await page.keyboard.type(env[currentEnv].username);
+    await page.click(selectors.username);
+    await page.keyboard.type(env[currentEnv].username);
 
-  await page.click(selectors.password);
-  await page.keyboard.type(env[currentEnv].password);
+    await page.click(selectors.password);
+    await page.keyboard.type(env[currentEnv].password);
 
-  await page.click(selectors.submit);
+    await page.click(selectors.submit);
 
-  await page.waitForNavigation();
+    await page.waitForNavigation()
+      .then(async (result) => {
+        await page.on('response', (data) => {
+          if(data._status === 200) {
+            resolve('Success');
+          }
+          else {
+            reject('Status:', data._status, data._statusText);
+          }
+        })
+      })
+      .catch( async (error) => {
+        if(selectors.error){
 
-  return true;
+          let loginError = await page.$$eval(errorSelector, errorElem => {
+            return errorElem.length;
+          });
+
+          if(loginError > 0) {
+            reject('Check your login & password');
+          }
+        }
+
+        reject(error);
+      });
+  });
+
+  return makeLoginPromise;
 }
 
 module.exports = {
