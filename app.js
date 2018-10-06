c1e67bcd339610cb019df9b0c52e38d3c7c1c265
yokselzok <yokselzok@gmail.com>
2018-10-07 02:11:40 +0300

    Add Express server

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+var bodyParser = require("body-parser");
+
+const dashboardPage = require('./pages/dashBoard');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+var jsonParser = bodyParser.json();
+
+app.use(express.static(__dirname + '/public'));
+app.get('/', dashboardPage);
+
+let httpServer = app.listen(PORT, () => {
+  console.log(`Server is running on localhost:${PORT}`);
+});
