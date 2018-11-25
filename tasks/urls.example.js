091e6a59ae47900678d0f1f8fffc4a6a81a4a31e
yokselzok <yokselzok@gmail.com>
2018-11-25 16:43:43 +0300

    Add ability to switch on beforeStart task; add urls task example

---
+++ tasks/urls.example.js
@@ -0,0 +1,38 @@
+const listId = 'urls';
+const listTitle = 'Discovery pages';
+
+const tasks = [
+  {
+    id: 'pages-finder',
+    name: 'Find all links on site',
+    creditsEnv: 'prod',
+    useBeforeStart: false,
+    startUrl: 'https://www.my-site.com/',
+    // Check this string in every url
+    domainRestriction: 'my-site.com/',
+    // Finds all links on page
+    // Set another selector to get links from particular element
+    linksSelectorRestriction: 'a',
+    // Max iterations for searchin links
+    max: 100,
+    // Enable if you need screenshots
+    makeScreens: true,
+    // if true, ignores height from screenSizes
+    screenFullPage: false,
+    ignoreStrings: [
+      '#',
+      'void(0)',
+      'mailto:',
+      'data/rss'
+    ],
+    findOnce: [
+      'calendar\\/year'
+    ]
+  }
+];
+
+module.exports = {
+  listId,
+  listTitle,
+  tasks
+};
